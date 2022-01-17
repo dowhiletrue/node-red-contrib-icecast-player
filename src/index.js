b1a293ba8189e0dd6fa5c11b01b5ce03283a4758
Michael Zaugg <zaugg@puzzle.ch>
2022-01-17 17:42:25 +0100

    add initial support to play back icecast resource

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const Parser = require("icecast-parser");
+const lame = require('@suldashi/lame');
+const Speaker = require('speaker');
+const query = 'querystring';
+
+const radioStation = new Parser({ url: "http://stream.srg-ssr.ch/m/regi_be_fr_vs/mp3_128", keepListen: true });
+
+const decoder = new lame.Decoder();
+
+const speaker = new Speaker({
+  channels: 2,
+  bitDepth: 16,
+  sampleRate: 44100,
+});
+
+radioStation.on('metadata', metadata => console.log(metadata));
+
+radioStation.on('stream', stream => stream.pipe(decoder).pipe(speaker)
+);
+
+
